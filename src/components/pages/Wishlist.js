ec61afd16d5cad8e83c7bedc5a272ec9f221e6d8
Farhaan14 <farhaanrtinwala@gmail.com>
2021-05-11 15:31:17 +0530

    init

--- src/components/pages/Wishlist.js
+++ src/components/pages/Wishlist.js
@@ -1,11 +1,34 @@
 import React from 'react';
 import Navbar from '../Navbar';
+import firebase from 'firebase';
+import 'firebase/storage';
+import 'firebase/firestore';
 
-export default function Wishlist(){
-    return(
-        <>
-            <Navbar />
-            <h1>Wishlist</h1>
-        </>
-    )
+
+class Wishlist extends React.Component{
+    componentDidMount(){
+        // var user = firebase.auth().currentUser;
+
+        // const projectFirestore = firebase.firestore();
+
+        // console.clear();
+        // if (user != null) {
+        //     console.log("UID: " + user.uid);
+        //     projectFirestore.collection(user.uid).doc("Players").set({
+        //         id: "ID",
+        //         name: "Name"
+        //     })
+            
+        // }
+    }
+    render(){
+        return(
+            <>
+                <Navbar />
+                <h1>Wishlist</h1>
+            </>
+        )
+    }
 }
+
+export default Wishlist;
--- src/components/pages/fire.js
+++ src/components/pages/fire.js
@@ -1,12 +1,25 @@
 import firebase from 'firebase';
+
+// var firebaseConfig = {
+//     apiKey: "AIzaSyD5s8CPE53OO747SoSEA36WOpwc2J77QKc",
+//     authDomain: "form2-accf3.firebaseapp.com",
+//     projectId: "form2-accf3",
+//     storageBucket: "form2-accf3.appspot.com",
+//     messagingSenderId: "31364164456",
+//     appId: "1:31364164456:web:3257304b55eecac079c4dd"
+//   };
+
 var firebaseConfig = {
-    apiKey: "AIzaSyD5s8CPE53OO747SoSEA36WOpwc2J77QKc",
-    authDomain: "form2-accf3.firebaseapp.com",
-    projectId: "form2-accf3",
-    storageBucket: "form2-accf3.appspot.com",
-    messagingSenderId: "31364164456",
-    appId: "1:31364164456:web:3257304b55eecac079c4dd"
-  };
+  apiKey: "AIzaSyCxG9Ge-THwGzIN2Q9KpSxeVQKauO4TIeE",
+  authDomain: "fut-stars.firebaseapp.com",
+  projectId: "fut-stars",
+  storageBucket: "fut-stars.appspot.com",
+  messagingSenderId: "495779158684",
+  appId: "1:495779158684:web:76d3f6db309a32c90e6b59"
+};
 
   const fire = firebase.initializeApp(firebaseConfig);
+
+
+
   export default fire;
